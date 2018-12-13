b91a1d593c0190c81fcae6a01600ccd68e7c00a3
Seth Kerr <skerr@aggies.ncat.edu>
2018-12-12 19:43:32 -0500

    Adding basic files to build on

---
+++ Py-Files/game-main.py
@@ -0,0 +1,7 @@
+# Author: Seth Kerr
+# Date: 12/12/2018
+#
+# This is the main file for the game. it will run basically everything aside from the level-generator.
+#
+# Start coding after this line:
+
---
+++ Py-Files/level-generator.py
@@ -0,0 +1,9 @@
+# Author: Seth Kerr
+# Date: 12/12/2018
+#
+# This is for generating the levels for the side scroller. Ideally these levels will be loaded into the main game file.
+#
+# We want to make a map that is a 2D array filled with towns, forrests, fields, rivers, cliffs, etc...
+#
+# Start code after this line:
+
